d5eebdebe6eb1e8bb1fbe92e6cae41ccb70d3b66
Daabid <benllochdavid10@gmail.com>
2025-06-04 15:36:25 +0200

    commit

--- routes/web.php
+++ routes/web.php
@@ -22,17 +22,6 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::get('/catalogo', [ProductController::class, 'index'])->name('catalogo');
 Route::get('/producto/{id}', [ProductController::class, 'show'])->name('producto.show'); //Página más información del producto
 
-//Rutas para el login
-Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/logout', [AuthController::class, 'logout']);
-
-Route::post('/register', [AuthController::class, 'register']);
-
-//Rutas para la tienda
-Route::get('/catalogo', [ProductController::class, 'index'])->name('catalogo');
-Route::get('/producto/{id}', [ProductController::class, 'show'])->name('producto.show'); //Página más información del producto
-
 // Paginaa de reserva de pista
 Route::get('/calendario', function()
 {
