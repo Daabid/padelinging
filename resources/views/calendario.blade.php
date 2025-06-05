f1448ace2351fbd89218a534efd1cbdad6373dd9
JXimFP <ximfullanapons@gmail.com>
2025-06-05 19:49:07 +0200

    juntar todo

--- resources/views/calendario.blade.php
+++ resources/views/calendario.blade.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html>
-    <header>
+    <head>
         <title> Calendario Reservas</title>
         <script defer src="{{ asset('js/calendario.js') }}"></script>
-    </header>
+    </head>
     <body>
         <p>Seleccion ala fecha en la que quieres reservar:</p>
-        <input type="dataTime" id="fDeseada">
+        <input type="date" id="fDeseada">
         <P>Selecciona la hora y la pistra que quieres:
         <table class="calendario">
             
         </table>
         <form class="verifiReserva">
-            
+            <!-- <input type="check"> -->
         </form>
     </body>
 </html>
